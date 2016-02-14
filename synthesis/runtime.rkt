f02a52a6cb276f2260f75bf451f6fd66c2e01248
Nathaniel Yazdani <nyazdani@cs.washington.edu>
2016-02-13 22:12:23 -0800

    New interpreter runtime representation
    Replaces the old runtime data structure with one that includes an explicit
    structure for each type as well as true operator overloading in an otherwise
    monomorphic type system.

--- synthesis/runtime.rkt
+++ synthesis/runtime.rkt
@@ -5,43 +5,47 @@
 
 (require "../utility.rkt")
 
-(provide (struct-out ftl-runtime)
+(provide (struct-out ftl-type)
+         (struct-out ftl-runtime)
          ftl-base-runtime)
 
-; an FTL runtime environment descriptor
-(struct ftl-runtime
+(define logic-unary
+  (hasheq '! !))
+
+(define logic-binary
+  (hasheq '&& &&
+          '|| ||))
+
+(define arithmetic-unary
+  (hasheq '- -))
+
+(define arithmetic-binary
+  (hasheq '+ +
+          '- -
+          '* *
+          '/ /))
+
+; FTL has a monomorphic type system with operator overloading
+
+(struct ftl-type ; type descriptor
+  (predicate ; test whether the given value is of this type
+   generate ; generate symbolic value, i.e., an oracle; may be void
+   equal? ; t*t -> boolean
+   less? ; t*t -> boolean
+   unary ; hasheq from symbols (e.g., -) to unary operations : t -> t
+   binary ; hasheq from symbols (e.g., +, -, *, /)  to binary operations : t*t -> t
+   ) #:transparent)
+
+(struct ftl-runtime ; description of an interpreted FTL runtime environment
   (boolean ; name of boolean type (symbol)
    types ; hash(eq) table from types (symbols) to pairs of value predicates and oracles (or void if unsupported)
    library ; hash(eq) table from symbols to pairs of types and functions, s.t. a type a * b -> c is '(a b c)
    ) #:transparent)
 
-; FIXME: distinguish operators from functions to allow overloading (perhaps just through an op->fn map?)
-; by necessaity, comparison operators require special handling, because they're of type t*t -> 2 rather than t*t -> t
-(define ftl-base-runtime
+(define ftl-base-runtime ; minimal runtime environment (basic arithmetic with no external functions)
   (ftl-runtime 'bool
-               (hasheq 'int (cons integer? hole*) ; (list integer? hole* = < (hasheq '+ + '* *...))
-                       'float (cons number? void)
-                       'bool (cons boolean? decide*)
-                       'string (cons string? void)) ; (list string? void string-append void void void equal?
-               (hasheq '+ `((int int int) . ,+)
-                       '- `((int int int) . ,-)
-                       '* `((int int int) . ,*)
-                       '/ `((int int int) . ,/)
-                       '== `((int int bool) . ,=)
-                       '< `((int int bool) . ,<)
-                       '> `((int int bool) . ,>)
-                       '<= `((int int bool) . ,<=)
-                       '>= `((int int bool) . ,>=)
-;                       the following infix operators are not supported by the parser but might/ought to be added
-;                       '+. `((float float float) . ,+)
-;                       '-. `((float float float) . ,-)
-;                       '*. `((float float float) . ,*)
-;                       '/. `((float float float) . ,/)
-;                       '==. `((float float bool) . ,=)
-;                       '<. `((float float bool) . ,<)
-;                       '>. `((float float bool) . ,>)
-;                       '<=. `((float float bool) . ,<=)
-;                       '>=. `((float float bool) . ,>=)
-                       'equal `((string string bool) . ,equal?)
-                       'concat `((string string string) . ,string-append)
-                       'length `((string string string) . ,string-length))))
+               (hasheq 'int (ftl-type integer? hole* = < arithmetic-unary arithmetic-binary)
+                       'float (ftl-type number? hole* = < arithmetic-unary arithmetic-binary)
+                       'bool (ftl-type boolean? decide* = void logic-unary logic-binary)
+                       'string (ftl-type string? void string=? string<? (hasheq) (hasheq '+ string-append)))
+               (hasheq 'length `((string int) . ,string-length))))
