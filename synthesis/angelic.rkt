339004b6ec7a7a76c31213db629f500d5c4a92a3
Nathaniel Yazdani <nyazdani@cs.washington.edu>
2016-02-13 21:28:48 -0800

    Remove uses of deleted symbol table data structure
    Also includes some minor refactoring, new function stubs, and documentation
    updates.

--- synthesis/angelic.rkt
+++ synthesis/angelic.rkt
@@ -27,28 +27,34 @@
   (evaluate derivation solution))
 
 (define (symbolize runtime grammar derivation)
-  (let* ([recurse (curry symbolize runtime grammar)]
-         [oracle (λ (type)
+  (let* ([oracle (λ (type)
                    (cdr (hash-ref (ftl-runtime-types runtime) type)))]
-         [vocab (ftl-ir-grammar-vocabulary grammar)]
-         [symbol (ftl-derivation-symbol derivation)]
-         [option (ftl-derivation-option derivation)]
-         [attributes (ftl-derivation-attributes derivation)]
-         [children (ftl-derivation-children derivation)]
-         [alternative (dict-ref (dict-ref vocab symbol) option)]
-         [labels (ftl-ir-alternative-labels alternative)]
-         [typeof (λ (label)
-                   (dict-ref labels label))])
-    (for-each (λ (label)
-                (when (void? (symtab-ref attributes label))
-                  (symtab-set! attributes label ((oracle (typeof label))))))
-              (symtab-keys attributes))
-    (for-each (λ (child)
-                (if (list? child)
-                    (for-each recurse child)
-                    (recurse child)))
-              (dict-values children))
-    derivation))
+         [vocab (ftl-ir-grammar-vocabulary grammar)])
+    (define (recurse node)
+      (let* ([symbol (ftl-derivation-symbol derivation)]
+             [option (ftl-derivation-option derivation)]
+             [bindings (ftl-derivation-attributes derivation)]
+             [children (ftl-derivation-children derivation)]
+             [alternative (hash-ref (hash-ref vocab symbol) option)]
+             [labels (ftl-ir-alternative-labels alternative)]
+             [typeof (λ (label)
+                       (hash-ref labels label))]
+             [noninputs (filter (λ (label)
+                                  (not (defined bindings label)))
+                                labels)]
+             [bind* (λ (label bindings)
+                      (cons (cons label ((oracle (typeof label))))
+                            bindings))]
+             [traverse (λ (child)
+                         (if (list? child)
+                             (map recurse child)
+                             (recurse child)))])
+        (ftl-derivation symbol
+                        option
+                        (foldl bind* bindings noninputs)
+                        (cdrmap traverse children))))
+
+    (recurse derivation)))
 
 ; FIXME: refactor
 (define (constrain grammar derivation) ; constrain angelic values
@@ -58,35 +64,35 @@
          [option (ftl-derivation-option derivation)]
          [attributes (ftl-derivation-attributes derivation)]
          [children (ftl-derivation-children derivation)]
-         [alternative (dict-ref (dict-ref vocab symbol) option)]
+         [production (hash-ref (hash-ref vocab symbol) option)]
          [sequences (ftl-ir-alternative-sequences alternative)]
          [definitions (ftl-ir-alternative-definitions alternative)]
          [listify (λ (l) (if (list? l) l (list l)))])
-    (dict-for-each definitions ; FIXME: actually verify that inputs, children given and of the correct types
+    (hash-for-each definitions ; FIXME: actually verify that inputs, children given and of the correct types
                    (λ (attribute action)
                      (let* ([def-obj (car attribute)]
                             [def-lbl (cdr attribute)]
-                            [def-seq (dict-has-key? sequences def-obj)]
+                            [def-seq (hash-has-key? sequences def-obj)]
                             [lbl-ref (λ (deriv label)
-                                       (symtab-ref (ftl-derivation-attributes deriv)
-                                                   label))]
+                                       (lookup (ftl-derivation-attributes deriv)
+                                               label))]
                             [reduce (ftl-ir-reduction? action)]
-                            [lookup (λ (child-attributes accumulator dependency)
+                            [refer (λ (child-attributes accumulator dependency)
                                       (let ([dep-obj (ftl-ir-dependency-object dependency)]
                                             [dep-idx (ftl-ir-dependency-index dependency)]
                                             [dep-lbl (ftl-ir-dependency-label dependency)])
                                         (cond
                                           [(and (eq? dep-obj def-obj)
                                                 def-seq
-                                                (eq? dep-idx 'current)) (symtab-ref child-attributes dep-lbl)]
+                                                (eq? dep-idx 'current)) (lookup child-attributes dep-lbl)]
                                           [(and (eq? dep-obj def-obj)
                                                 reduce
                                                 (eq? dep-idx 'previous)) accumulator]
                                           [(and (eq? dep-obj 'self)
-                                                (eq? dep-idx 'none)) (symtab-ref attributes dep-lbl)]
-                                          [(eq? dep-idx 'none) (lbl-ref (dict-ref children dep-obj) dep-lbl)]
-                                          [(eq? dep-idx 'first) (lbl-ref (first (dict-ref children dep-obj)) dep-lbl)]
-                                          [(eq? dep-idx 'last) (lbl-ref (last (dict-ref children dep-obj)) dep-lbl)])))]
+                                                (eq? dep-idx 'none)) (lookup attributes dep-lbl)]
+                                          [(eq? dep-idx 'none) (lbl-ref (lookup children dep-obj) dep-lbl)]
+                                          [(eq? dep-idx 'first) (lbl-ref (first (lookup children dep-obj)) dep-lbl)]
+                                          [(eq? dep-idx 'last) (lbl-ref (last (lookup children dep-obj)) dep-lbl)])))]
                             [echo-assert (λ (b)
                                            (display "assertion: ")
                                            (displayln b)
@@ -98,32 +104,32 @@
                                   [step (ftl-ir-reduction-step-define action)]
                                   [step-eval (ftl-ir-definition-evaluate step)]
                                   [step-deps (ftl-ir-definition-dependencies step)]
-                                  [iter (dict-ref children (ftl-ir-reduction-iter-child action))]
+                                  [iter (lookup children (ftl-ir-reduction-iter-child action))]
                                   [proc (λ (child accum)
                                           (let* ([child-attributes (ftl-derivation-attributes child)]
-                                                 [result (step-eval (vector-map (curry lookup child-attributes accum)
+                                                 [result (step-eval (vector-map (curry refer child-attributes accum)
                                                                                 step-deps))])
                                             (when def-seq
-                                              (echo-assert (equal? (symtab-ref child-attributes def-lbl) result))
+                                              (echo-assert (equal? (lookup child-attributes def-lbl) result))
                                               result)))]
-                                  [seed (init-eval (vector-map (curry lookup (void) (void)) init-deps))]
+                                  [seed (init-eval (vector-map (curry refer (void) (void)) init-deps))]
                                   [result (foldl proc seed iter)])
                              (unless def-seq
-                               (echo-assert (equal? (lbl-ref (dict-ref children def-obj) def-lbl)
+                               (echo-assert (equal? (lbl-ref (lookup children def-obj) def-lbl)
                                                     result))))
                            (let* ([eval (ftl-ir-definition-evaluate action)]
                                   [deps (ftl-ir-definition-dependencies action)]
                                   [iter (listify (if (eq? def-obj 'self)
                                                      derivation
-                                                     (dict-ref children def-obj)))]
+                                                     (lookup children def-obj)))]
                                   [proc (λ (child)
                                           (let* ([child-attributes (ftl-derivation-attributes child)]
-                                                 [result (eval (vector-map (curry lookup child-attributes (void))
+                                                 [result (eval (vector-map (curry refer child-attributes (void))
                                                                            deps))])
-                                            (echo-assert (equal? (symtab-ref child-attributes def-lbl) result))))])
+                                            (echo-assert (equal? (lookup child-attributes def-lbl) result))))])
                              (for-each proc iter))))))
     (for-each (λ (child)
                 (if (list? child)
                     (for-each recurse child)
                     (recurse child)))
-              (dict-values children))))
+              (map cdr children))))
--- synthesis/derivation.rkt
+++ synthesis/derivation.rkt
@@ -13,10 +13,10 @@
          example-deriv)
 
 (struct ftl-derivation
-  (symbol ; key from vocabulary
-   option ; identifier of production instance
-   attributes ; symbol table of labels to values, such that all input attributes are set and the rest are (void)
-   children ; dictionary of IR derivations (or lists of them) keyed by child names
+  (symbol ; nonterminal symbol from vocabulary
+   option ; identifier of production alternative for this nonterminal
+   attributes ; list associating labels with values, such that an attribute is defined initially iff it is an input
+   children ; list associating child names with (lists of) derivations (all objects except 'self)
    ) #:transparent)
 
 ; TODO: read derivation from XML
@@ -31,14 +31,47 @@
 (define (derive* grammar height)
   (let* ([symbol (choose* (hash-keys grammar))]
          [option (choose* (hash-keys (hash-ref grammar symbol)))])
-    (ftl-ir-derivation symbol
-                       option
-                       (void)
-                       (void))))
+    (ftl-derivation symbol
+                    option
+                    (void)
+                    (void))))
 
-; ------------
-; Demo program
-; ------------
+; -------------------------
+; Traversals of derivations
+; -------------------------
+
+; FIXME: how to control or specify "short-circuited" traversals?
+; Pertaining to the above, is the conservative method of aborting a
+; traversal if the subtree is a certain nonterminal sufficient? How
+; about including an 'abort'/'short-circuit'/'guard' parameter to the
+; traversal functions below, to decide whether to continue the traversal
+; as a function of the current node (usually the nonterminal and the
+; production)?
+
+; child must be a sequence
+; accumulator is list associating $- attributes to values
+; initial : accumulator
+; visit-parent : accumulator * node -> accumulator
+; visit-child : accumulator * node * node -> accumulator
+; TODO: in-order (i.e., "recursive") traversal
+(define (inorder tree child initial visit-parent visit-child)
+  (void))
+
+; visit-parent : node -> void
+; visit-child : node * node -> void
+; TODO: pre-order traversal
+(define (preorder tree child visit-parent visit-child)
+  (void))
+
+; visit-parent : node -> void
+; visit-child : node * node -> void
+; TODO: post-order traversal
+(define (postorder tree child visit-parent visit-child)
+  (void))
+
+; -------------------------------------
+; Example derivation of example grammar
+; -------------------------------------
 
 (define example-xml
 "
