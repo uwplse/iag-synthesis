090a509ddeb28860c93505fdd42d03e560bff20b
= <cnandi@cs.washignton.edu>
2016-02-26 18:43:23 -0800

    interpreter working for a simple grammar, not generating code, but interpreting and executing it

--- synthesis/codegen.rkt
+++ synthesis/codegen.rkt
@@ -7,7 +7,9 @@
 
 ;; Consider the following attribute grammar:
 (define example-grammar "
- interface Top {}
+ interface Top {
+     var x: int;
+   }
    interface Node {
      var a: int ;
    }
@@ -18,6 +20,7 @@
      }
 
      actions{
+       x := 5;
        child.a := x;     
      }
    }
@@ -41,126 +44,112 @@
 ;; (later, this should be read from a sched file, parsed and stored in a racket data structure automatically):
 (define example-schedule '([TD (Root child a) (Midnode left a) (Midnode right a)]))
 
-;; Example derivation tree : node name, class, interface, attributes, children
-(define example-tree '(root Root Top '() '(
-                                           (child Midnode Node '(a #f) '(
-                                                                        (left Midnode Node '(a  #f) '()) (right Midnode Node '(a  #f) '()))))))
-;; Functions to access different tree elements
-(define node-name car)
-(define node-class cadr)
-(define node-interface caddr)
-(define node-attributes cadddr)
-(define node-children last)
+;; Structs for representing derivation trees : each node has a name, class, interface, and zero or more attributes and children
+(struct parse_tree
+  (name
+   parent_name
+   class
+   interface
+   attrs
+   children) #:mutable #:transparent)
 
-(define (set-attribute! n attr-name val)
-  (map (lambda (p) (if (equal? attr-name (car p)) (set-box! (cdr p) val) (void))) (cadr(node-attributes n)))
-  (void))
+(struct attribute
+  (name
+   (value #:mutable)
+   ) #:transparent)
 
-(define (get-attribute n attr-name) (cdr (assoc attr-name (cadr(node-attributes n)))))
- 
-(define codegen-file "generated-code.rkt")
+;; Example derivation tree
+(define example-tree (parse_tree
+                      'root
+                      'none
+                      'Root
+                      'Top
+                      (list (attribute 'x 5))
+                      (list
+                       (parse_tree
+                        'child
+                        'root
+                        'Midnode
+                        'Node
+                        (list (attribute 'a 0))
+                        (list
+                         (parse_tree
+                           'left
+                           'child
+                           'Midnode
+                           'Node
+                           (list(attribute 'a 0))
+                           '())
+                          (parse_tree
+                           'right
+                           'child
+                           'Midnode
+                           'Node
+                           (list(attribute 'a 0))
+                           '()))))))
 
-(define code (open-output-file codegen-file #:exists 'truncate))
+(define generated-code (list))
 
-(display "#lang racket" code)
-(newline code) (newline code)
+(define (set-attr! att val)
+  (set-attribute-value! att val))
 
-(display "(define node-name car)
-(define node-class cadr)
-(define node-interface caddr)
-(define node-attributes cadddr)
-(define node-children last)
+;; go through the schedule and get the attributes assigned in TD or BU manner. Then call another method to actually do the assignment.
+(define (gen-code schedule grammar tree)
+ (map (λ(x)
+       (cond
+         [(equal? (car x) 'TD)
+          (let ([node-attr (cdr x)]) (do-td-assignment node-attr grammar tree))] 
+         [(equal? (car x) 'BU)
+          (let ([node-attr (cdr x)]) (do-bu-assignment node-attr grammar tree))])) schedule) (void))
 
-(define (set-attribute! n attr-name val)
-  (map (lambda (p) (if (equal? attr-name (car p)) (set-box! (cdr p) val) (void))) (cadr(node-attributes n)))
-  (void))
+(define (do-td-assignment attr grammar tree)
+ (map (λ(x)
+        (cond
+          [(and (not (null? tree))(equal? (parse_tree-name tree) (cadr x))) 
+           (cond
+             [(not (null? (parse_tree-attrs tree)))
+              (assign (caddr x) grammar tree)])]
+          [(not (equal? (parse_tree-name tree) (cadr x)))
+           (map (λ(y) (do-td-assignment attr grammar y)) (parse_tree-children tree))])) attr))
 
-(define (get-attribute n attr-name) (cdr (assoc attr-name (cadr(node-attributes n)))))
+(define (do-bu-assignment attr grammar tree)
+  (map (λ(x)
+         ((map (λ(y) (do-bu-assignment attr grammar y)) (parse_tree-children tree))
+          (cond
+            [(and (not (null? tree)) (equal? (parse_tree-name tree) (cadr x)))
+             (cond
+               [(not (null?(parse_tree-attrs tree)))
+                (assign (caddr x) grammar tree)])]))) attr))
 
-" code)
+(define (assign attr-name grammar node)
+  (map (λ(x)
+         (cond
+           [(ftl-ast-class? x)
+            (traverse-actions attr-name (ftl-ast-body-actions (ftl-ast-class-body x)) node)])) grammar))
 
-(display "(define tree " code)
-(display example-tree code)
-(display ")"code)
+(define (traverse-actions attr-name class-actions node)
+  (map(λ(x)
+        (cond
+          [(and (and (equal? (parse_tree-name node) (ftl-ast-refer-object (ftl-ast-define-lhs x))) (equal? attr-name (ftl-ast-refer-label (ftl-ast-define-lhs x))))
+                (not (equal? (parse_tree-name node) 'self)))
+           (cond 
+             [(equal? (ftl-ast-refer-object (ftl-ast-define-rhs x)) 'self)
+              (map (λ(y)
+                     (cond
+                       [(equal? (attribute-name y) attr-name)
+                        (find-parent-and-assign y example-tree (parse_tree-parent_name node) (ftl-ast-refer-label(ftl-ast-define-rhs x)))])) (parse_tree-attrs node))])])) class-actions))
 
-(newline code) (newline code)
-;; To generate functions for different traversals by parsing the FTL generated schedule. This schedule is stored in a racket data structure for convenience.
-;; At the moment this works for TD and BU.
-(define (generate-code code-file schedule tree grammar)
-  (define traversal-to-function (map (λ(i)
-                                       (display (string-append "(define (" (string-append (string-downcase(symbol->string (car i))) "_assign ")) code)
-                                       (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                       (display "_assignment" code)
-                                       (display " tree)" code) (newline code)
-                                       (display " (unless (null? tree) or (null?(node-attributes tree))" code) (newline code)
-                                       (cond
-                                         [(string=? (string-downcase(symbol->string (car i))) "td")
-                                          (display " (assign_" code)
-                                          (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                          (display " " code)
-                                          (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                          (display "_assignment tree)" code) (newline code)
-                                          (display " (map (λ (child) (" code)
-                                          (display (string-append (string-downcase(symbol->string (car i))) "_assign ") code)
-                                          (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                          (display "_assignment child)) (cadr(node-children tree)))))" code) (newline code) (newline code)]                                         
-                                         [(string=? (string-downcase(symbol->string (car i))) "bu")
-                                          (display "(map (λ (child) (" code)
-                                          (display (string-append (string-downcase(symbol->string (car i))) "_assign ") code)
-                                          (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                          (display "_assignment child)) (cadr(node-children tree)))" code)(newline code)
-                                          (display "(assign_" code)
-                                          (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                          (display " " code)
-                                          (display (symbol->string(car(cdr(cdr(cadr i))))) code)
-                                          (display "_assignment tree)))" code)])) schedule))
-  (newline code)
-  (attr-assign-function tree grammar))
+(define (find-parent-and-assign node_attr entire-tree parent-name assignment-rhs)
+  (cond
+  [(equal? parent-name (parse_tree-name entire-tree))
+   (map(λ(i)
+         (cond
+           [(equal? (attribute-name i) assignment-rhs)
+            (set-attr! node_attr (attribute-value i))
+            (display (attribute-value node_attr))])) (parse_tree-attrs entire-tree))]
+  [(not (equal? parent-name (parse_tree-name entire-tree)))
+   (map (λ(j)
+          (unless null? j)
+          (find-parent-and-assign node_attr j parent-name assignment-rhs)) (parse_tree-children entire-tree))]))
 
-;; To generate attribute assignment functions.
-;; TODO: handle other types of rhs expressions
-;; TODO: handle case where ftl-ast-class has attributes too
-;; TODO: handle traits
-;; TODO: handle loops
-(define (attr-assign-function tree grammar)
-  (map (λ(x)(cond
-              [(ftl-ast-interface? x)
-               (map (λ(i)
-                      (display "(define (assign_" code)
-                      (display (symbol->string(ftl-ast-declare-name i)) code)
-                      (display " " code)
-                      (display (symbol->string(ftl-ast-declare-name i)) code)
-                      (display " tree)" code) (newline code)
-                      (display "(cond " code)
-                      (newline code)
-                      (map (λ(y)
-                             (cond
-                               [(ftl-ast-class? y) 
-                                (map (λ(j) (cond
-                                             [(equal? (ftl-ast-refer-label(ftl-ast-define-lhs j)) (ftl-ast-declare-name i))
-                                              (cond
-                                                [(ftl-ast-refer? (ftl-ast-define-rhs j))
-                                                 (cond
-                                                   [(equal? (symbol->string(ftl-ast-refer-object(ftl-ast-define-rhs j))) "self")
-                                                     (display "[(equal? (node-class tree) " code)
-                                                    (display (ftl-ast-class-name y) code)
-                                                    ])
-                                                 (display ") " code)
-                                                 (display "(map (λ(w) (set-attribute! w " code)
-                                                 (display (symbol->string(ftl-ast-declare-name i)) code) (display " " code)
-                                                 (cond
-                                                   [(not(equal? (ftl-ast-refer-label(ftl-ast-define-rhs j)) (ftl-ast-declare-name i)))
-                                                    (display (symbol->string(ftl-ast-refer-label(ftl-ast-define-rhs j))) code)]
-                                                   [(equal? (ftl-ast-refer-label(ftl-ast-define-rhs j)) (ftl-ast-declare-name i))
-                                                    (display " (get-attribute tree " code)
-                                                    (display (symbol->string(ftl-ast-refer-label(ftl-ast-define-rhs j))) code)
-                                                    (display ")" code)])                                                   
-                                                 (display ")) (node-children tree))]" code)]
-                                                 (newline code))                                                 
-                                              (newline code)])) (ftl-ast-body-actions (ftl-ast-class-body y)))])) grammar)
-                      (display "))" code)
-                      (newline code) (newline code)) (ftl-ast-interface-fields x))])) grammar)) 
-
-(define sample-codegen (generate-code code example-schedule example-tree parsed-example-grammar))
-
-(close-output-port code)
+(define codegen (gen-code example-schedule parsed-example-grammar example-tree))
