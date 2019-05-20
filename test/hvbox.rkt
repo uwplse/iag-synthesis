fdc24fd18bc7d952fbc074feff48ed9ca2361f40
Nate Yazdani <nyazdani@cs.washington.edu>
2019-05-20 09:09:33 -0700

    Update HVBox test

--- test/hvbox.rkt
+++ test/hvbox.rkt
@@ -3,54 +3,52 @@
 ; Script to test on HVBox.
 
 (require racket/pretty
-         "../engine.rkt"
-         "../schedule/elaborate.rkt"
-         "../backend/rust/builder.rkt"
-         "../backend/rust/printer.rkt")
+         "../grammar/parse.rkt"
+         "../schedule/parse.rkt"
+         "../tree.rkt"
+         "../tracing/synthesizer.rkt"
+         ;"../backend/rust/builder.rkt"
+         ;"../backend/rust/printer.rkt"
+         )
 
 (displayln "Parsing attribute grammar...")
-(define hvbox-grammar (read-grammar "benchmarks/hvbox/hvbox.grammar" 'Top))
+(define hvbox-grammar (file->grammar "benchmarks/hvbox/hvbox.grammar"))
 
 (displayln "Generating attribute trees...")
-(define hvbox-examples (tree-examples hvbox-grammar))
+(define hvbox-examples (tree-examples hvbox-grammar 'Top))
 
 ; The schedule sketch for which to synthesize a completion.
 (define hvbox-sketch
-  (sched-sequential (sched-traversal 'post
-                                     `((Root ,(sched-hole))
-                                       (HBox ,(sched-hole) ,(sched-hole))
-                                       (VBox ,(sched-hole) ,(sched-hole))
-                                       (Leaf ,(sched-hole))))
-                    (sched-traversal 'pre
-                                     `((Root ,(sched-hole))
-                                       (HBox ,(sched-hole))
-                                       (VBox ,(sched-hole))
-                                       (Leaf ,(sched-hole))))))
+  `(seq (trav post
+              ((Root (recur root) (hole))
+               (HBox (recur childs) (iter-left childs ((hole))) (hole))
+               (VBox (recur childs) (iter-left childs ((hole))) (hole))
+               (Leaf (hole))))
+        (trav pre
+              ((Root (hole) (recur root))
+               (HBox (hole) (iter-left childs ((hole))) (recur childs))
+               (VBox (hole) (iter-left childs ((hole))) (recur childs))
+               (Leaf (hole))))))
 
 
 ; Another schedule sketch, this time using parallel composition.
 (define hvbox-sketch-parallel
-  (sched-parallel hvbox-sketch hvbox-sketch))
+  `(par ,hvbox-sketch ,hvbox-sketch))
 
 ; Synthesis with dataflow-tracing symbolic evaluation.
-(define (tracing:test-hvbox)
-  (tracing:complete-sketch hvbox-grammar (make-hole-range hvbox-grammar)
-                           hvbox-sketch hvbox-examples))
-
-; Synthesis with general-purpose symbolic evaluation (Rosette's engine).
-(define (checking:test-hvbox)
-  (checking:complete-sketch hvbox-grammar (make-hole-range hvbox-grammar)
-                            hvbox-sketch hvbox-examples))
+(define (test-hvbox)
+  (complete-sketch hvbox-grammar hvbox-sketch hvbox-examples))
 
 (newline)
 (displayln "Synthesizing a schedule to complete sketch...")
-(define hvbox-schedule (tracing:test-hvbox))
+(define hvbox-schedule (test-hvbox))
 (when hvbox-schedule
-  (display-schedule hvbox-schedule)
-  (newline)
-  (displayln "Elaborating synthesized schedule...")
-  (let ([hvbox-program (elaborate-schedule hvbox-grammar hvbox-schedule)])
-    (pretty-print hvbox-program)
-    (newline)
-    (displayln "Generating a Rust program to implement synthesized schedule...")
-    (rust:print (build-program hvbox-grammar hvbox-program))))
+  (displayln (schedule->string hvbox-schedule))
+;  (newline)
+;  (displayln "Elaborating synthesized schedule...")
+;  (let ([hvbox-program (elaborate-schedule hvbox-grammar hvbox-schedule)])
+;    (pretty-print hvbox-program)
+;    (newline)
+;    (displayln "Generating a Rust program to implement synthesized schedule...")
+;    (rust:print (build-program hvbox-grammar hvbox-program)))
+  )
