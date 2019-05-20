4f39293558aadc011b16826053cf256635288bd7
Yu Feng <fengyu8299@gmail.com>
2019-05-20 13:17:39 -0700

    minor

--- test/collapse.rkt
+++ test/collapse.rkt
@@ -16,16 +16,20 @@
 
 (displayln "Generating attribute trees...")
 (define collapse-examples (tree-examples collapse-grammar 'Block))
+(for ([t collapse-examples])
+(displayln t)
+
+)
 
 ; The schedule sketch for which to synthesize a completion.
 (define collapse-sketch
   `(seq (trav post
               ((Root (recur root) (hole))
-               (Node (recur kids) (iter-left kids ((hole))) (hole))
+               (Node (recur kids) (iter-left kids ((hole))) (iter-right kids ((hole))) (hole))
                (Leaf (hole))))
         (trav pre
               ((Root (hole) (recur root))
-               (Node (hole) (iter-left kids ((hole))) (recur kids))
+               (Node (hole) (iter-left kids ((hole))) (iter-right kids ((hole))) (recur kids))
                (Leaf (hole))))))
 
 ; Synthesis with dataflow-tracing symbolic evaluation.
