b0edfd82239e1c076dc660f4a46e81de5fb77f12
Nate Yazdani <nyazdani@cs.washington.edu>
2019-05-21 21:45:04 -0700

    Fix bug in AG formalization of margin collapse
    A specific interface must exist for the tree root.

--- test/collapse.rkt
+++ test/collapse.rkt
@@ -15,22 +15,40 @@
 (define collapse-grammar (file->grammar "benchmarks/css/collapse.grammar"))
 
 (displayln "Generating attribute trees...")
-(define collapse-examples (tree-examples collapse-grammar 'Block))
-(for ([t collapse-examples])
-(displayln t)
-
-)
+(define collapse-examples (tree-examples collapse-grammar 'Tree))
 
 ; The schedule sketch for which to synthesize a completion.
 (define collapse-sketch
   `(seq (trav post
-              ((Root (recur root) (hole))
-               (Node (recur kids) (iter-left kids ((hole))) (iter-right kids ((hole))) (hole))
-               (Leaf (hole))))
+              ((Root
+                (hole)
+                (recur root)
+                (hole))
+               (Node
+                (hole)
+                (iter-right kids ((hole)))
+                (hole)
+                (recur kids)
+                (hole)
+                (iter-right kids ((hole)))
+                (hole))
+               (Leaf
+                (hole))))
         (trav pre
-              ((Root (hole) (recur root))
-               (Node (hole) (iter-left kids ((hole))) (iter-right kids ((hole))) (recur kids))
-               (Leaf (hole))))))
+              ((Root
+                (hole)
+                (recur root)
+                (hole))
+               (Node
+                (hole)
+                (iter-right kids ((hole)))
+                (hole)
+                (recur kids)
+                (hole)
+                (iter-right kids ((hole)))
+                (hole))
+               (Leaf
+                (hole))))))
 
 ; Synthesis with dataflow-tracing symbolic evaluation.
 (define (test-collapse)
@@ -40,12 +58,4 @@
 (displayln "Synthesizing a schedule to complete sketch...")
 (define collapse-schedule (test-collapse))
 (when collapse-schedule
-  (displayln (schedule->string collapse-schedule))
-;  (newline)
-;  (displayln "Elaborating synthesized schedule...")
-;  (let ([hvbox-program (elaborate-schedule hvbox-grammar hvbox-schedule)])
-;    (pretty-print hvbox-program)
-;    (newline)
-;    (displayln "Generating a Rust program to implement synthesized schedule...")
-;    (rust:print (build-program hvbox-grammar hvbox-program)))
-  )
+  (displayln (schedule->string collapse-schedule)))
