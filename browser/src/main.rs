b7ee1c7dd7a8fee98dd5450705095591d61e72c2
Nate Yazdani <nyazdani@cs.washington.edu>
2019-08-03 18:01:02 -0700

    Organize defaults for browser CLI

--- browser/src/main.rs
+++ browser/src/main.rs
@@ -12,6 +12,12 @@ pub mod layout;
 pub mod style;
 pub mod paint;
 
+const DEFAULT_WIDTH: usize = 1920;
+const DEFAULT_HEIGHT: usize = 980;
+const DEFAULT_DOCUMENT: &str = "examples/test.html";
+const DEFAULT_STYLE: &str = "examples/test.css";
+const DEFAULT_OUTPUT: &str = "output.png";
+
 fn main() {
     // Parse command-line options:
     let mut opts = getopts::Options::new();
@@ -30,12 +36,12 @@ fn main() {
     };
 
     // Read input files:
-    let html = fs::read_to_string(str_arg("d", "examples/test.html")).unwrap();
-    let css  = fs::read_to_string(str_arg("s", "examples/test.css")).unwrap();
+    let html = fs::read_to_string(str_arg("d", DEFAULT_DOCUMENT)).unwrap();
+    let css  = fs::read_to_string(str_arg("s", DEFAULT_STYLE)).unwrap();
 
     // Configure viewport size:
-    let width  = num_arg("w", 800);
-    let height = num_arg("h", 600);
+    let width  = num_arg("w", DEFAULT_WIDTH);
+    let height = num_arg("h", DEFAULT_HEIGHT);
 
     // Parsing and rendering:
     let root_node = html::parse(html);
@@ -45,7 +51,7 @@ fn main() {
     let display_list = layout::display_list(&layout_root);
 
     // Create the output file:
-    let filename = str_arg("o", "output.png");
+    let filename = str_arg("o", DEFAULT_OUTPUT);
     let mut file = BufWriter::new(File::create(&filename).unwrap());
 
     // Write to the file:
