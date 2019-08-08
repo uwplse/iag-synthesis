84e2a37afe45ed1f391aa34d66b8707ac105bd80
Yu Feng <fengyu8299@gmail.com>
2019-08-08 10:08:11 -0700

    009

--- browser/examples/sanity/margin-collapse-009.rkt
+++ browser/examples/sanity/margin-collapse-009.rkt
@@ -2,33 +2,15 @@
 
 (define-stylesheet doc-1
   ((tag div)
-   [font-style normal]
-   #;[font-variant-caps normal]
-   [font-weight normal]
-   #;[font-stretch normal]
-   [font-size (px 20)]
-   [line-height (em 1)]
-   [font-family "Ahem"]
-   #;[font-size-adjust none]
-   #;[font-kerning auto]
-   #;[font-optical-sizing auto]
-   #;[font-variant-alternates normal]
-   #;[font-variant-east-asian normal]
-   #;[font-variant-ligatures normal]
-   #;[font-variant-numeric normal]
-   #;[font-variant-position normal]
-   #;[font-language-override normal]
-   #;[font-feature-settings normal]
-   #;[font-variation-settings normal]
-   [height (em 5)]
-   [width (em 5)])
+   [height (px 5)]
+   [width (px 5)])
   ((id div1)
-   [height (em 2)]
-   [margin-top (em 2)]
+   [height (px 2)]
+   [margin-top (px 2)]
    [overflow-x hidden]
    [overflow-y hidden])
   ((id div2)
-   [height (em 1)]
+   [height (px 1)]
    [background-color red]
    #;[background-position-x (% 0)]
    #;[background-position-y (% 0)]
@@ -38,19 +20,18 @@
    #;[background-size auto]
    #;[background-origin padding-box]
    #;[background-clip border-box]
-   [margin-top (em 2)]))
+   [margin-top (px 2)]))
 
 (define-fonts doc-1
-  [16 "serif" 400 normal 12 4 0 0 19.2]
-  [20 "Ahem" 400 normal 15 5 0 0 24])
+  [16 "serif" 400 normal 12 4 0 0 19.2])
 
-(define-layout (doc-1 :matched true :w 1280 :h 703 :fs 16 :scrollw 0)
+(define-layout (doc-1 :matched true :w 1280 :h 737 :fs 16 :scrollw 0)
  ([VIEW :w 1280]
-  ([BLOCK :x 0 :y 0 :w 1280 :h 148 :elt 0]
-   ([BLOCK :x 8 :y 40 :w 1264 :h 100 :elt 3]
-    ([BLOCK :x 8 :y 40 :w 100 :h 100 :elt 4]
-     ([BLOCK :x 8 :y 40 :w 100 :h 40 :elt 5]
-      ([BLOCK :x 8 :y 80 :w 100 :h 20 :elt 6])))))))
+  ([BLOCK :x 0 :y 0 :w 1280 :h 21 :elt 0]
+   ([BLOCK :x 8 :y 8 :w 1264 :h 5 :elt 3]
+    ([BLOCK :x 8 :y 8 :w 5 :h 5 :elt 4]
+     ([BLOCK :x 8 :y 8 :w 5 :h 2 :elt 5]
+      ([BLOCK :x 8 :y 10 :w 5 :h 1 :elt 6])))))))
 
 (define-document doc-1
   ([html :num 0]
@@ -68,5 +49,5 @@
   :fonts doc-1
   :documents doc-1
   :layouts doc-1
-  :features css:font-size css:overflow-x css:overflow-y float:0)
+  :features css:overflow-x css:overflow-y float:0)
 
